fe225b2c6789711fa1adb30ef1ff86f11045b760
wfoschiera <wfoschiera@gmail.com>
2021-02-16 23:50:37 -0400

    melhorada a saida de informacoes

--- code/utils/db_manager.py
+++ code/utils/db_manager.py
@@ -21,31 +21,33 @@ def cep_loader(csv_file='../data/ceps_sem_localidades.csv', db_path='../data/cep
     cur = conn.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS Pareamento (cep TEXT, localidade TEXT)''')
     df = pd.read_csv(csv_file, sep=',')
-
+    count_news = 0
+    count_exists = 0
     for index, row in df.iterrows():
         cep = row['CEP']
         cur.execute("SELECT cep FROM Pareamento WHERE cep= ?", (cep,))
 
         try:
             data = cur.fetchone()[0]
-            print("Found in database ", cep)
+            #print("Found in database ", cep)
             # continue jumps to next iteration if cep is found in db
+            count_exists += 1
             continue
         except:
             pass
         # if len(cep) != 8, its not a valid cep
         if not len(str(cep)) == 8:
             continue
-        print('Resolving', cep)
 
         try:
             address = get_address_from_cep(cep, webservice=WebService.CORREIOS)
             localidade = address['bairro']
+            count_news += 1
         except:
             localidade = 'Cep nao encontrado'
 
-        print(localidade)
         cur.execute('''INSERT INTO Pareamento (cep, localidade) 
                 VALUES ( ?, ? )''', (cep, localidade))
 
         conn.commit()
+    print(f'CEPs existentes no banco: {count_exists}\nCEPs adcionados: {count_news}')
